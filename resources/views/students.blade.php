87128738714ab027cc5380256b67eea1fd2e0163
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2021-08-18 02:14:38 +0600

    update record using ajax

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -32,15 +32,19 @@
                                         <th>Last Name</th>
                                         <th>Email</th>
                                         <th>Phone</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach($students as $student)
-                                    <tr>
+                                    <tr id="sid{{$student->id}}">
                                         <td>{{$student->firstname}}</td>
                                         <td>{{$student->lastname}}</td>
                                         <td>{{$student->email}}</td>
                                         <td>{{$student->phone}}</td>
+                                        <td>
+                                            <a href="javascript:void(0)" onclick="editStudent({{$student->id}})" class="btn btn-info">Edit</a>
+                                        </td>
                                     </tr>
                                     @endforeach
                                 </tbody>
@@ -52,7 +56,7 @@
         </div>
     </section>
 
-    <!-- Modal -->
+    <!-- Add New Student Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -92,7 +96,46 @@
         </div>
     </div>
 
-
+    <!-- Edit Student Modal -->
+    <div class="modal fade" id="exampleEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add New Student</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="studentEditForm">
+                        @csrf
+                        <input type="hidden" name="id" id="id">
+                        <div class="form-group">
+                            <label for="firstname">First Name</label>
+                            <input type="text" name="" id="firstname2" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="lastname">Last Name</label>
+                            <input type="text" name="" id="lastname2" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" name="" id="email2" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone</label>
+                            <input type="text" name="" id="phone2" class="form-control">
+                        </div>
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-success mt-2">Submit</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
@@ -139,6 +182,50 @@
                     }
                 }
             });
+        });
+    </script>
+
+    <script>
+        function editStudent(id) {
+            $.get('/students/'+id, function(student){
+                $("#id").val(student.id);
+                $("#firstname2").val(student.firstname);
+                $("#lastname2").val(student.lastname);
+                $("#email2").val(student.email);
+                $("#phone2").val(student.phone);
+                $("#exampleEditModal").modal('toggle');
+            });
+        }
+
+        $("#studentEditForm").submit(function(e){
+            e.preventDefault();
+            let id = $("#id").val();
+            let firstname = $("#firstname2").val();
+            let lastname = $("#lastname2").val();
+            let email = $("#email2").val();
+            let phone = $("#phone2").val();
+            let _token = $("input[name=_token]").val();
+            
+            $.ajax({
+                url: '{{route("student.update")}}',
+                type: "PUT",
+                data: {
+                    id: id,
+                    firstname: firstname,
+                    lastname: lastname,
+                    email: email,
+                    phone: phone,
+                    _token: _token,
+                },
+                success: function(response){
+                    $('#sid' + response.id +' td:nth-child(1)').text(response.firstname);
+                    $('#sid' + response.id +' td:nth-child(2)').text(response.lastname);
+                    $('#sid' + response.id +' td:nth-child(3)').text(response.email);
+                    $('#sid' + response.id +' td:nth-child(4)').text(response.phone);
+                    $("#exampleEditModal").modal('toggle');
+                    $("#studentEditForm")[0].reset();
+                }
+            })
         });
     </script>
 </body>
